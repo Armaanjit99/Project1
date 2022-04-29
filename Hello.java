43648f48f23a2a9ff264544a7372bc81c5bbb722
Armaanjit99 <armaanjit1999@gmail.com>
2022-04-29 16:33:25 +0530

    done2

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ class Hello {
 	public static void main(String[] args) {
 	System.out.println("Hello! World");
 	System.out.println("Welcome");
+	System.out.println("Welcome to Jenkins");
+	System.out.println("Integration of git with Jenkins is done");
 }
 } 
